722353debb68639eb5c6697d3b7f638659269b02
SpiritCorder <60997354+SpiritCorder@users.noreply.github.com>
2023-12-16 09:13:30 +0530

    Configured react router & setup basic routes along with their corresponding pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+// page components
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
+
 const App = () => {
-  return <h1 className="text-red-600">Hello World</h1>;
+  return (
+    <>
+      <Routes>
+        {/* Register Page */}
+        <Route path="/register" element={<RegisterPage />} />
+        {/* Login Page */}
+        <Route path="/login" element={<LoginPage />} />
+        {/* Home Page */}
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
