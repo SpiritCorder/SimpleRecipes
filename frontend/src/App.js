ccaa77b757a336d00efd4fa43ee9895f4c1f08fd
SpiritCorder <60997354+SpiritCorder@users.noreply.github.com>
2023-12-17 12:18:30 +0530

    Finished create recipe process & added toast notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // page components
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
@@ -11,6 +14,7 @@ import AuthRoute from "./components/utils/AuthRoute";
 const App = () => {
   return (
     <>
+      <ToastContainer position="top-center" />
       <Navbar />
       <Routes>
         {/* Add New Recipe Page */}
